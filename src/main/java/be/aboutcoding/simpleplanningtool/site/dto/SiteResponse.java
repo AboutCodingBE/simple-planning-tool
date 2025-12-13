35e934dd9a17805c19a9dc11b43aa91cdacd3074
Nicholas Ocket <nicholas@aboutcoding.be>
2025-12-13 13:42:43 +0100

    small changes

--- src/main/java/be/aboutcoding/simpleplanningtool/site/dto/SiteResponse.java
+++ src/main/java/be/aboutcoding/simpleplanningtool/site/dto/SiteResponse.java
@@ -1,7 +1,9 @@
 package be.aboutcoding.simpleplanningtool.site.dto;
 
+import be.aboutcoding.simpleplanningtool.site.Customer;
+import be.aboutcoding.simpleplanningtool.site.Site;
+import be.aboutcoding.simpleplanningtool.worker.Worker;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.springframework.cglib.core.Local;
 
 import java.time.Instant;
 import java.time.LocalDate;
@@ -17,4 +19,45 @@ public record SiteResponse(
         @JsonProperty("duration_in_days") Integer durationInDays,
         List<WorkerResponse> workers
 ) {
+    public static SiteResponse from(Site site) {
+        CustomerResponse customerResponse = from(site.getCustomer());
+        List<WorkerResponse> workers = from(site.getWorkers());
+
+        return new SiteResponse(
+                site.getId(),
+                site.getName(),
+                customerResponse,
+                site.getDesiredDate(),
+                site.getExecutionDate(),
+                site.getCreationDate(),
+                site.getDurationInDays(),
+                workers
+        );
+    }
+
+    private static CustomerResponse from(Customer customer) {
+        if (customer == null) {
+            return null;
+        }
+
+        return new CustomerResponse(
+                customer.getId(),
+                customer.getName(),
+                customer.getIsPrivate()
+        );
+    }
+
+    private static List<WorkerResponse> from(List<Worker> workers){
+        if (workers == null) {
+            return List.of();
+        }
+
+        return workers.stream()
+                .map(worker -> new WorkerResponse(
+                        worker.getId(),
+                        worker.getFirstName(),
+                        worker.getFirstName()))
+                .toList();
+    }
+
 }
